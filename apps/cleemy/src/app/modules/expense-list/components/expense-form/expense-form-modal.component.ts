657b16126483f540083e268d0f79230df365e58b
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-28 01:29:09 +0200

    feat: improve expense form modal & add section for foreign currencies

--- apps/cleemy/src/app/modules/expense-list/components/expense-form/expense-form-modal.component.ts
+++ apps/cleemy/src/app/modules/expense-list/components/expense-form/expense-form-modal.component.ts
@@ -1,43 +1,30 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { NzModalRef } from 'ng-zorro-antd/modal';
 import Currencies from '../../../shared/utils/currencies.json';
 import { Expense } from '../../models/expense.model';
+import { ExpenseService } from '../../store/expense.service';
+import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'cleemy-expense-form-modal',
   templateUrl: './expense-form-modal.component.html',
   styleUrls: ['./expense-form-modal.component.scss'],
 })
-export class ExpenseFormModalComponent implements OnInit {
+export class ExpenseFormModalComponent implements OnInit, OnDestroy {
+  @Output() submitExpense: EventEmitter<Partial<Expense>> = new EventEmitter<Partial<Expense>>();
+
   validateForm!: UntypedFormGroup;
-  currencies: any = Object.keys(Currencies);
+  currencies: string[] = Currencies;
   editedExpense: Partial<Expense> | undefined;
+  private destroy$ = new Subject();
 
-  @Output() onConfirm: EventEmitter<Partial<Expense>> = new EventEmitter<Partial<Expense>>();
-
-  constructor(private fb: UntypedFormBuilder, public modal: NzModalRef) {}
+  constructor(private fb: UntypedFormBuilder, private expenseService: ExpenseService) {}
 
   submitForm(): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
       if (this.validateForm.valid) {
-        const expense = {
-          ...(this.editedExpense ?? null),
-          purchasedOn: this.validateForm.value.purchasedOn,
-          nature: this.validateForm.value.nature,
-          comment: this.validateForm.value.comment,
-          originalAmount: {
-            amount: this.validateForm.value.originalAmount,
-            currency: this.validateForm.value.originalCurrency,
-          },
-          convertedAmount: {
-            amount: this.validateForm.value.convertedAmount,
-            currency: this.validateForm.value.conversionCurrency,
-          },
-        };
-
-        this.onConfirm.emit(expense);
-
+        const expense = this.buildFormattedExpense();
+        this.submitExpense.emit(expense);
         resolve(true);
       } else {
         Object.values(this.validateForm.controls).forEach((control) => {
@@ -46,7 +33,6 @@ export class ExpenseFormModalComponent implements OnInit {
             control.updateValueAndValidity({ onlySelf: true });
           }
         });
-
         reject(false);
       }
     });
@@ -57,14 +43,60 @@ export class ExpenseFormModalComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+
+  convertAmount(originalAmount: string | number | undefined) {
+    console.log(originalAmount);
+    if (originalAmount?.toString().length === 0) {
+      this.validateForm.get('convertedAmount')?.setValue(0);
+      return;
+    }
+
+    const originalCurrency: any = this.validateForm.get('originalCurrency')?.value;
+    this.expenseService
+      .convertExpense({
+        amount: originalAmount,
+        from: originalCurrency,
+        to: 'EUR',
+      })
+      .pipe(takeUntil(this.destroy$), debounceTime(50000), distinctUntilChanged())
+      .subscribe((convertedAmount) => {
+        this.validateForm.get('convertedAmount')?.setValue(Math.round(convertedAmount.result));
+      });
+  }
+
+  private buildForm() {
     this.validateForm = this.fb.group({
       purchasedOn: [this.editedExpense?.purchasedOn ?? null, [Validators.required]],
       nature: [this.editedExpense?.nature ?? null, [Validators.required]],
       comment: [this.editedExpense?.comment ?? null, [Validators.required]],
-      originalAmount: [this.editedExpense?.originalAmount?.amount ?? null, [Validators.required]],
-      originalCurrency: [this.editedExpense?.originalAmount?.currency ?? 'USD', [Validators.required]],
+      originalAmount: [this.editedExpense?.originalAmount?.amount ?? null],
+      originalCurrency: [this.editedExpense?.originalAmount?.currency ?? 'USD'],
       convertedAmount: [this.editedExpense?.convertedAmount?.amount ?? null, [Validators.required]],
       conversionCurrency: [this.editedExpense?.convertedAmount?.currency ?? 'EUR', [Validators.required]],
     });
+  }
+
+  private buildFormattedExpense() {
+    return {
+      ...(this.editedExpense ?? null),
+      purchasedOn: this.validateForm.value.purchasedOn,
+      nature: this.validateForm.value.nature,
+      comment: this.validateForm.value.comment,
+      originalAmount: {
+        amount: this.validateForm.value.originalAmount,
+        currency: this.validateForm.value.originalCurrency,
+      },
+      convertedAmount: {
+        amount: this.validateForm.value.convertedAmount,
+        currency: this.validateForm.value.conversionCurrency,
+      },
+    };
   }
 }
