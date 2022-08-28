f2222546bb9de6f9b2a71a436e1df4de2cee34c7
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-28 23:31:31 +0200

    feat: create http interceptor to handle total count of expenses

---
+++ apps/cleemy/src/app/core/interceptors/http.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
+import { Store } from '@ngxs/store';
+import { ExpenseActions } from '../../modules/expense-list/store/expense.actions';
+
+@Injectable()
+export class CleemyInterceptor implements HttpInterceptor {
+  constructor(private store: Store) {}
+
+  intercept(httpRequest: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(httpRequest).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse && event.headers.has('X-Total-Count')) {
+          this.store.dispatch(
+            new ExpenseActions.UpdateTotalCount(parseInt(event.headers.get('X-Total-Count') as string)),
+          );
+        }
+        return event;
+      }),
+      catchError((error: any) => {
+        return throwError(error);
+      }),
+    );
+  }
+}
