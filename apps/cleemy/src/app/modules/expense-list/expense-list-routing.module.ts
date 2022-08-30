de45f59888966e849f5e63e2ecc04f2190dc1013
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-30 13:33:31 +0200

    refacto: remove unused routes

--- apps/cleemy/src/app/modules/expense-list/expense-list-routing.module.ts
+++ apps/cleemy/src/app/modules/expense-list/expense-list-routing.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExpenseListComponent } from './expense-list.component';
-import { NotFoundComponent } from '../shared/components/not-found/not-found.component';
-import { ExpenseFormModalComponent } from './components/expense-form/expense-form-modal.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ExpenseListComponent,
-  },
-  {
-    path: 'not-found',
-    component: NotFoundComponent,
-  },
-  {
-    path: ':expenseId',
-    component: ExpenseFormModalComponent,
   },
 ];
 
