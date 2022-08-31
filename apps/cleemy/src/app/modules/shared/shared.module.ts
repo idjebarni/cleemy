87ee0aa0961cb277837a1819e317d760144c0aab
imad-dh <74598949+imad-dh@users.noreply.github.com>
2022-08-31 14:45:35 +0200

    refacto: create custom ellipsis pipe

--- apps/cleemy/src/app/modules/shared/shared.module.ts
+++ apps/cleemy/src/app/modules/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { EllipsisPipe } from './pipes/ellipsis.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [EllipsisPipe],
   imports: [CommonModule, RouterModule],
+  exports: [EllipsisPipe],
 })
 export class SharedModule {}
